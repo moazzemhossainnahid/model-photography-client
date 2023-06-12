62e924c7364dd195fa567423e2d8b2819fa9bbc5
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-06-13 00:25:37 +0600

    add stats

--- src/Pages/ContactUS/ContactUS.jsx
+++ src/Pages/ContactUS/ContactUS.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ContactDetails from '../../Components/ContactUS/ContactDetails';
+import ContactStep from '../../Components/ContactUS/ContactStep';
 
 const ContactUS = () => {
     return (
         <div className="container w-full py-10 px-6">
             <ContactDetails />
+            <ContactStep/>
         </div>
     );
 };
