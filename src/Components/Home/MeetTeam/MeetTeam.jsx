ca0ab5f8779ec57944d4a1e309901bc1ad096763
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-06-13 20:02:46 +0600

    add team

--- src/Components/Home/MeetTeam/MeetTeam.jsx
+++ src/Components/Home/MeetTeam/MeetTeam.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Slide } from 'react-reveal';
+import { FaBars, FaFacebook, FaLinkedin, FaTwitter, FaYoutube } from "react-icons/fa";
+import { TeamsData } from '../../../Data/TeamsData';
 
 const MeetTeam = () => {
     return (
@@ -19,6 +21,27 @@ const MeetTeam = () => {
                     <p className="w-full">Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.</p>
                 </div>
             </Slide>
+            <div className="w-full grid grid-cols-1 md:grid-cols-4 gap-7 p-3 pt-7">
+                {
+                    TeamsData?.map(d => (
+                        <div key={d?.id} className="">
+                            <Slide bottom duration={2000}>
+                                <img src={d?.imageURL} alt="" className="w-full" />
+                            </Slide>
+                                <h5 style={{ fontFamily: "Rajdhani" }} class="text-2xl capitalize md:text-3xl font-bold"><Slide bottom duration={1500}>{d?.name}</Slide></h5> 
+                                <h5 style={{ fontFamily: "Rajdhani" }} class="text-md font-semibold"><Slide bottom duration={2000}>{d?.title}</Slide></h5>
+                            <Slide bottom duration={2500}>
+                                <div className="w-full flex gap-5 pt-3 justify-center items-center">
+                                    <FaFacebook className='text-md' />
+                                    <FaYoutube className='text-md' />
+                                    <FaTwitter className='text-md' />
+                                    <FaLinkedin className='text-md' />
+                                </div>
+                            </Slide>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     );
 };
---
+++ src/Data/TeamsData.jsx
@@ -0,0 +1,52 @@
+export const TeamsData = [
+    {
+        id:1,
+        imageURL: 'https://estelle.qodeinteractive.com/wp-content/uploads/2019/04/team-img-1.jpg',
+        name:"ASHLEY RAMOS",
+        title:"Make Up Artist",
+        social:{
+            facebook:"",
+            youtube:"",
+            twitter:"",
+            linkedin:"",
+        }
+    },
+    {
+        id:2,
+        imageURL: 'https://estelle.qodeinteractive.com/wp-content/uploads/2019/04/team-img-2.jpg',
+        name:"ANDREA ALLEN",
+        title:"Hairdresser",
+        social:{
+            facebook:"",
+            youtube:"",
+            twitter:"",
+            linkedin:"",
+        }
+    },
+    {
+        id:3,
+        imageURL: 'https://estelle.qodeinteractive.com/wp-content/uploads/2019/04/team-img-3.jpg',
+        name:"ROSE HAYES",
+        title:"Make Up Artist",
+        social:{
+            facebook:"",
+            youtube:"",
+            twitter:"",
+            linkedin:"",
+        }
+    },
+    {
+        id:4,
+        imageURL: 'https://estelle.qodeinteractive.com/wp-content/uploads/2019/04/team-img-4.jpg',
+        name:"CRAIG DOUGLAS",
+        title:"Stylist",
+        social:{
+            facebook:"",
+            youtube:"",
+            twitter:"",
+            linkedin:"",
+        }
+    },
+
+
+];
