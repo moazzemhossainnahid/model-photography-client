2a0d813c04979f52cf075c2b47df63331157fc93
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-06-13 13:49:14 +0600

    add new faces

--- src/Components/Home/NewFaces/NewFaces.jsx
+++ src/Components/Home/NewFaces/NewFaces.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Slide } from 'react-reveal';
+import { NewFacesData } from '../../../Data/NewFacesData';
 
 const NewFaces = () => {
     return (
@@ -19,6 +20,23 @@ const NewFaces = () => {
                     <p className="w-full">Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.</p>
                 </div>
             </Slide>
+            <div className="grid grid-cols-2 md:grid-cols-5 py-7">
+                {
+                    NewFacesData?.map(d => (
+                        <div key={d?.id} className="">
+                            <div className="relative group overflow-hidden">
+                                <img src={d?.img} alt="" className="group relative" />
+                                <div className="absolute cursor-pointer translate-y-80 group-hover:translate-y-0 ease-in-out duration-500 w-full h-full top-0 flex flex-col gap-3 justify-center items-center left-0 bg-[#000000a9]">
+                                <h5 style={{ fontFamily: "Rajdhani" }} class="text-2xl capitalize text-white font-bold"><Slide bottom duration={1500}>{d?.title}</Slide></h5>
+                                <Slide bottom duration={2000}>
+                                <h5 className="text-md text-gray-200">{d?.category}</h5>    
+                                </Slide>                            
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     );
 };
